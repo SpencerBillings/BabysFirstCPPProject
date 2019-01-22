9ee08362f428a397d5b5e2d61dca282cc918a084
Spencer B <spencerbillings2000@gmail.com>
2019-01-22 09:11:31 -0700

    Created the project and tested code in the main.cpp file

---
+++ BabysFirstCPPProject/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  BabysFirstCPPProject
+//
+//  Created by Billings, Spencer on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hallo Weld!\n";
+}
